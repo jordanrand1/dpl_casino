dccff7c8f63dd1790fe1205f10c9985c53cbc5e2
Nate <nathanestes@gmail.com>
2018-08-17 16:15:00 -0600

    commit at 415 Friday

---
+++ go_fish.rb
@@ -0,0 +1,83 @@
+require_relative 'casino_manager.rb'
+require_relative 'deck.rb'
+
+#require gemfile
+##what gems may be useful?
+
+
+class GoFish
+
+  def initialize#(manager)
+    #@manager = manager
+    d = Deck.new
+    #d.list
+  end
+
+  def deal
+    puts 'this is deal'
+    pid = fork{ exec 'afplay', "song.mp3" }
+  end
+    #7 random cards to each player
+    #dealt cards cannot be used again
+    #create array for each player to hold dealt cards
+    # .sample copies, doesn't delete
+  #   loop over deck.arr using .pop or .slice to remove values in .sample (player hands)
+  # .clone #to save the original array 
+  # .rand
+
+
+  def hand
+    puts 'this is hand'
+    #this will track each players hands dynamically
+    #string for each??
+  end
+
+  def rules
+    puts 'Paste this Into Your browser'
+    puts 'https://www.bicyclecards.com/how-to-play/go-fish/'
+    puts
+    puts 'Press Enter to Return to Main Menu'
+    puts
+    gets
+    gf_menu
+  end
+
+  def gf_menu
+    puts '------------------------'
+    puts 'Welcome to Go Fish!'
+    puts '1) Review the Rules'
+    puts '2) Deal Cards'
+    puts '3) Exit'
+    puts '------------------------'
+    menu_choice
+    #gets
+    #take user input to initialize deal
+    #deal
+    #gf_menu
+  end
+
+  def menu_choice
+    choice = gets.strip.to_i
+    case choice
+      when 1 
+        rules
+        menu_choice
+      when 2
+        deal
+        menu_choice
+      when 3
+        exit #exit to main_casino menu
+      else
+        puts 'whats your problem man'
+      end
+  end
+end
+
+while true 
+gf = GoFish.new
+gf.gf_menu
+end
+
+
+#call methods and classes here
+#gf_menu
